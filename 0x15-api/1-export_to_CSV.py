98448f6a30e3d93e8f9cb722ae72da873a852c93
Gareth Brickman <977@holbertonschool.com>
2020-02-04 03:05:11 +0000

    [TASK] 1.

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Queries REST API for employee info, exports to CSV
+    "USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
+    argv 1 = int employee ID
+"""
+if __name__ == "__main__":
+    import csv
+    import requests as r
+    from sys import argv
+
+    user_id = argv[1]
+    # Finds employee username by "id" param in /users/
+    name_q = r.get("https://jsonplaceholder.typicode.com/users/{}/"
+                   .format(user_id))
+    data = name_q.json()
+    username = data.get("username")
+
+    # Finds employee tasks by "userID" param; /users/ & /todo/ are linked
+    url = "https://jsonplaceholder.typicode.com/users/1/todos/"
+    task_q = r.get(url, params={'userId': user_id})
+    data = task_q.json()
+
+    # Creates csv file, writes request data into it row by row
+
+    with open('{}.csv'.format(user_id), 'w', newline='') as csvfile:
+        csv_data = csv.writer(csvfile, delimiter=',', quotechar='"',
+                              quoting=csv.QUOTE_ALL)
+        csv_data.writerow(["userId", "completed", "title"])
+        for elem in data:
+            csv_data.writerow([elem["userId"], username, elem["completed"],
+                               elem["title"]])
